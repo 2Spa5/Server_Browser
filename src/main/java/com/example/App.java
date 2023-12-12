8681e94230d73e9d3e81e08ec5a783251491c6f6
lai2-24 <lai2-24@itismeucci.com>
2023-12-12 14:31:56 +0100

    Caricamento Immagine Mancante

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,16 @@
 package com.example;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
-import java.util.Scanner;
 
 /**
  * Hello world!
@@ -30,6 +33,8 @@ public class App {
                 path = path.substring(1);
                 System.out.println("----" + path + "----");
 
+                String name = findFile(path);
+
                 do {
                     String line = in.readLine();
                     System.out.println(line);
@@ -37,7 +42,7 @@ public class App {
                         break;
                 } while (true);
 
-                sendFile(out, path);
+                sendBinaryFile(s, name, findExt(name));
 
                 out.flush();
                 s.close();
@@ -49,27 +54,61 @@ public class App {
         }
     }
 
-    private static void sendFile(PrintWriter out, String file) {
+    private static String findFile (String path) {
         try {
-            File myObj = new File("test.html");
-            Scanner myReader = new Scanner(myObj);
+            String p[] = path.split("/");
+            String s = p[1];
+            return s;
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return "ERROR";
+    }
 
-            out.println("HTTP/1.1 200 OK");
-            out.println("Content-Lenght " + myObj.length());
-            out.println("Server: Java HTTP Server from Spagni: 1.0");
-            out.println("Date: " + new Date());
-            out.println("Content-Type: text/html; charset=utf-8");
+    private static String findExt (String path) {
+        try {
+            String a[] = path.split("\\.");
+            path = a[1];
+            return path;
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return "ERROR";
+    }
 
-            out.println();
+    private static void sendBinaryFile(Socket socket, String name, String ext) throws IOException {
+        try {
+            String filename = "htdocs/" + name;
+            File file = new File(filename);
 
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                out.println(data);
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            out.writeBytes("HTTP/1.1 200 OK\n");
+            out.writeBytes("Content-Lenght " + file.length() + "\n");
+            out.writeBytes("Server: Java HTTP Server from Spagni: 1.0\n");
+            out.writeBytes("Date: " + new Date() + "\n");
+
+            if (ext == "jpg" || ext == "jpeg" || ext == "png")
+                out.writeBytes("Content-Type: image/" + ext + "; charset=utf-8\n");
+            if (ext == "html")
+                out.writeBytes("Content-Type: text/html; charset=utf-8\n");
+            if (ext == "css")
+                out.writeBytes("Content-Type: text/css; charset=utf-8\n");
+            if (ext == "js")
+                out.writeBytes("Content-Type: application/js; charset=utf-8\n");
+            
+            InputStream in = new FileInputStream(file);
+            byte b[] = new byte[8192];
+            int n;
+            while ((n = in.read(b)) != -1) {
+                out.write(b, 0, n);
             }
-            myReader.close();
+            in.close();
 
         } catch (Exception e) {
-            out.println("HTTP/1.1 404 OK");
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            out.writeBytes("HTTP/1.1 404 Not Found");
         }
     }
+
 }
